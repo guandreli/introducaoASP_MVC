cb826c538c1142d22d47eb90b483d958c89bd02d
Gustavo Andreli <gu_andreli@hotmail.com>
2018-01-11 14:42:29 -0200

    introdução com algumas paradas a mais

---
+++ Introducao/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Introducao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public ActionResult Index() {
+            var pessoa = new Pessoa();
+            pessoa.id = 0;
+            pessoa.nome = "Gustavo";
+            pessoa.codTipo = 2;
+            pessoa.tipoPessoa(pessoa.codTipo);
+
+            ViewBag.id = pessoa.id;
+            ViewBag.nome = pessoa.nome;
+            ViewBag.tipoPessoa = pessoa.usuario;
+            return View(pessoa);
+        }
+
+        public ActionResult Contact() {
+            return View();
+        }
+
+        public ActionResult About() {
+            return View();
+        }
+
+        public ActionResult Teste() {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa) {
+            pessoa.tipoPessoa(pessoa.codTipo);
+            return View(pessoa);
+        }
+    }
+}
---
+++ Introducao/Models/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Introducao.Models {
+    public class Pessoa {
+        public int id;
+        public string nome { get; set; }
+        public int codTipo { get; set; }
+        public string usuario { get; set; }
+
+        public void tipoPessoa (int codTipo) {
+            if(codTipo == 1) {
+                usuario = "Administrador";
+            }
+            else {
+                usuario = "Usuario";
+            }
+        }
+
+    }
+}
